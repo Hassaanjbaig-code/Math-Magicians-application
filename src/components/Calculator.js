0291e6aec2465b9503ec29b314c39a0d2fe8be0c
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-01-10 20:00:19 +0500

    Structure

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import './Calculator.css';
 
-class Calculator extends React {
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.show = {};
+  }
+
   render() {
     return (
-      <div className='main'>
-        <div></div>
+      <div className="calculator">
+        <ul className="main">
+          <input type="text" id="Text" className="text" readOnly />
+          <li className="row">
+            <button type="button">AC</button>
+            <button type="button">+/-</button>
+            <button type="button">%</button>
+            <button type="button" className="right">/</button>
+          </li>
+          <li className="row">
+            <button type="button">7</button>
+            <button type="button">8</button>
+            <button type="button">9</button>
+            <button type="button" className="right">*</button>
+          </li>
+          <li className="row">
+            <button type="button">4</button>
+            <button type="button">5</button>
+            <button type="button">6</button>
+            <button type="button" className="right">-</button>
+          </li>
+          <li className="row">
+            <button type="button">1</button>
+            <button type="button">2</button>
+            <button type="button">3</button>
+            <button type="button" className="right">+</button>
+          </li>
+          <li className="row">
+            <button type="button" className="zero">0</button>
+            <button type="button">.</button>
+            <button type="button" className="right">=</button>
+          </li>
+        </ul>
       </div>
     );
   }
