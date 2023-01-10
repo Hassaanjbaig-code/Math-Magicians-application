3ff2e147ec9de206ee93b1a243c5f0d1da5d2864
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-01-10 18:39:14 +0500

    Erase unnecessary and setup of class

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Calculator extends React {
+  render() {
+    return (
+      <div className='main'>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
