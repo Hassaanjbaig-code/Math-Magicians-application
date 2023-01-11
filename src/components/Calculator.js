2862b1fad88b251ba2ef596626a9cab2f38d969b
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-01-11 21:12:29 +0500

    Add calculate and operate

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,45 +1,57 @@
+/* eslint-disable */
 import React from 'react';
 import './Calculator.css';
+import calculate from './logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.show = {};
+    this.show = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick (e) {
+    console.log(e.target.innerText);
+    const btn = e.target.innerText;
+    calculate(this.show,btn);
   }
 
   render() {
     return (
       <div className="calculator">
         <ul className="main">
-          <input type="text" id="Text" className="text" readOnly />
+          <input type="text" id="Text" value={this.show.total} className="text" readOnly />
           <li className="row">
-            <button type="button">AC</button>
-            <button type="button">+/-</button>
-            <button type="button">%</button>
-            <button type="button" className="right">/</button>
+            <button onClick={this.handleClick} type="button">AC</button>
+            <button onClick={this.handleClick} type="button">+/-</button>
+            <button onClick={this.handleClick} type="button">%</button>
+            <button onClick={this.handleClick} type="button" className="right">/</button>
           </li>
           <li className="row">
-            <button type="button">7</button>
-            <button type="button">8</button>
-            <button type="button">9</button>
-            <button type="button" className="right">*</button>
+            <button onClick={this.handleClick} type="button">7</button>
+            <button onClick={this.handleClick} type="button">8</button>
+            <button onClick={this.handleClick} type="button">9</button>
+            <button onClick={this.handleClick} type="button" className="right">*</button>
           </li>
           <li className="row">
-            <button type="button">4</button>
-            <button type="button">5</button>
-            <button type="button">6</button>
-            <button type="button" className="right">-</button>
+            <button onClick={this.handleClick} type="button">4</button>
+            <button onClick={this.handleClick} type="button">5</button>
+            <button onClick={this.handleClick} type="button">6</button>
+            <button onClick={this.handleClick} type="button" className="right">-</button>
           </li>
           <li className="row">
-            <button type="button">1</button>
-            <button type="button">2</button>
-            <button type="button">3</button>
-            <button type="button" className="right">+</button>
+            <button onClick={this.handleClick} type="button">1</button>
+            <button onClick={this.handleClick} type="button">2</button>
+            <button onClick={this.handleClick} type="button">3</button>
+            <button onClick={this.handleClick} type="button" className="right">+</button>
           </li>
           <li className="row">
-            <button type="button" className="zero">0</button>
-            <button type="button">.</button>
-            <button type="button" className="right">=</button>
+            <button onClick={this.handleClick} type="button" className="zero">0</button>
+            <button onClick={this.handleClick} type="button">.</button>
+            <button onClick={this.handleClick} type="button" className="right">=</button>
           </li>
         </ul>
       </div>
