8a75a3a916d766be57542994f7e839113e578341
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-01-13 13:44:11 +0500

    Convert the class to function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Calculator from './components/Calculator';
+import Fcalculator from './components/Fcalculator';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,7 +12,7 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <Calculator />
+        <Fcalculator />
       </>
     );
   }
