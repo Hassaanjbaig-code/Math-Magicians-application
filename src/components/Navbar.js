945586b674522db4bfc48f7a83e3bd87c55314f8
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-01-17 22:23:36 +0500

    Working of pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/about',
+      text: 'About',
+    },
+    {
+      id: 3,
+      path: '/Quote',
+      text: 'Quotes',
+    },
+  ];
+  return (
+    <nav className="nav-bar">
+      <ul className="nav-menu">
+        {links.map((link) => (
+          <li key={link.id}>
+            <NavLink to={link.path} className="nav-item">
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <h3>Welcome to over page</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+      sint temporibus necessitatibus iste omnis aliquid officia mollitia
+      deleniti? Laudantium, dolor dolores. Vitae ut tempore molestias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor facere,
+      dolores iusto veritatis quaerat officiis minima repellat, a nesciunt
+      doloribus saepe, quasi numquam temporibus!
+    </p>
+  </div>
+);
+
+export default About;
